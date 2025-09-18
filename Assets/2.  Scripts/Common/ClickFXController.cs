e27c82d422a649c66026357ff12b281bf1f8cf1e
sbc605 <sbc605@naver.com>
2025-09-18 11:25:48 +0900

    클릭이펙트 수정

--- Assets/2.  Scripts/Common/ClickFXController.cs
+++ Assets/2.  Scripts/Common/ClickFXController.cs
@@ -10,6 +10,7 @@ public class ClickFXController : MonoBehaviour
     [SerializeField] private Sprite[] frames;
 
     [SerializeField] private SpriteRenderer board; // 오목판
+    [SerializeField] private Transform resultPanel; // 결과 패널
 
     void Update()
     {
@@ -17,7 +18,7 @@ public class ClickFXController : MonoBehaviour
     }
 
     void ScreentTouch()
-    {       
+    {
 
 #if UNITY_EDITOR || UNITY_STANDALONE // 유니티 에디터, PC 마우스 클릭
         if (Input.GetMouseButtonDown(0))
@@ -36,13 +37,14 @@ public class ClickFXController : MonoBehaviour
     }
 
     void CreateEffect(Vector2 pos)
-    {
-        if (EventSystem.current.IsPointerOverGameObject()) return; // 버튼 위 이펙트 생성 방지
-
-        Vector3 worldPos = Camera.main.ScreenToWorldPoint(pos);
-        worldPos.z = board.transform.position.z;
-
-        if (board.bounds.Contains(worldPos)) return; // 오목판 내부는 이펙트 생성x
+    {       
+        // 결과 패널이 비활성화된 상태
+        if (!IsResultPanelActive())
+        {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(pos);
+            worldPos.z = board.transform.position.z;
+            if (board.bounds.Contains(worldPos)) return; // 오목판 내부는 이펙트 생성x
+        }
 
         var effect = Instantiate(effectPrefab, canvas.transform);
         effect.rectTransform.localScale = Vector3.one * 0.5f;
@@ -63,5 +65,16 @@ public class ClickFXController : MonoBehaviour
             yield return new WaitForSeconds(time);
         }
         Destroy(effect.gameObject);
+    }
+
+    private bool IsResultPanelActive()
+    {
+        foreach (Transform child in resultPanel)
+        {
+            if (child.gameObject.activeSelf)
+                return true;
+        }
+
+        return false;
     }
 }
